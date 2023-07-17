ccfd1045fa6300100fb463690685f1d983585ccf
David Lopez <davidlopez.hellin@outlook.com>
2023-07-17 20:17:57 +0200

    refactor: added CLI verbose argument and refactored validation of user input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use clap::Parser;
+use semver::Version;
 use std::{
     io::Error,
     io::ErrorKind,
     process::{exit, Command},
 };
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Run with verbose mode
+    #[arg(short, long, default_value = "false", default_missing_value = "true")]
+    verbose: bool,
+}
+
+// const PATCH_INCREASE_TYPES: [&'static str; 1] = ["fix"];
+// const MINOR_INCREASE_TYPES: [&'static str; 3] = ["feat", "refactor", "pref"];
 
 fn verify_git() -> Result<bool, Error> {
     Command::new("git").arg("--version").output()?;
@@ -91,16 +104,83 @@ fn get_commit_messages(from_commit: &String, to_commit: &String) -> Result<Vec<S
 }
 
 fn main() {
-    if let Err(error) = verify_git() {
-        println!("Git verification not passed, error: {}", error);
-        exit(1);
+    let args = Args::parse();
+
+    match verify_git() {
+        Err(error) => {
+            println!("git verification not passed, error: {}", error);
+            exit(1);
+        }
+        Ok(_) => {
+            if args.verbose {
+                println!("found git installation and git managed project")
+            };
+        }
     }
 
-    let tag = get_closest_tag().expect("failed to get closest tag");
-    println!("tag: {}", tag);
-    let tag_commit_sha = get_tag_commit_sha(&tag).expect("failed to get tag commit sha");
-    println!("tag {} - commit: {}", tag, tag_commit_sha);
-    let commits_messages = get_commit_messages(&tag_commit_sha, &String::from("HEAD"))
-        .expect("could not get commit messages");
-    println!("{:?}", commits_messages);
+    let closest_tag = match get_closest_tag() {
+        Err(error) => {
+            println!("could not get a tag, error: {}", error);
+            exit(1);
+        }
+        Ok(tag) => {
+            if args.verbose {
+                println!("closest tag found: {}", tag);
+            }
+            tag
+        }
+    };
+
+    let version = match Version::parse(&closest_tag) {
+        Err(error) => {
+            println!(
+                "could not parse tag '{}' as a semantic version, error: {}",
+                closest_tag, error
+            );
+            exit(1);
+        }
+        Ok(version) => {
+            println!("current version: {}", version);
+            version
+        }
+    };
+
+    let tag_commit_sha = match get_tag_commit_sha(&closest_tag) {
+        Err(error) => {
+            println!(
+                "could not get commit sha for tag '{}', error: {}",
+                closest_tag, error
+            );
+            exit(1);
+        }
+        Ok(commit_sha) => {
+            if args.verbose {
+                println!(
+                    "commit sha of commit pointed by tag '{}': {}",
+                    closest_tag, commit_sha
+                )
+            }
+            commit_sha
+        }
+    };
+
+    let head_commit = String::from("HEAD");
+    let commit_messages = match get_commit_messages(&tag_commit_sha, &head_commit) {
+        Err(error) => {
+            println!(
+                "failed to get commit messages from commit '{}' to commit '{}', error: {}",
+                tag_commit_sha, head_commit, error
+            );
+            exit(1);
+        }
+        Ok(commit_messages) => {
+            if args.verbose {
+                println!("extracted commit messages from commit '{}' to commit '{}', list of messages: {:?}", tag_commit_sha, head_commit, commit_messages);
+            }
+            commit_messages
+        }
+    };
+
+    println!("{:?}", commit_messages);
+    println!("{:?}", version)
 }
