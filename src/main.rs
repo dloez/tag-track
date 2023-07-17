4df5270bdb00ba1073ce74da6d5131a258524f43
David Lopez <davidlopez.hellin@outlook.com>
2023-07-17 16:47:07 +0200

    feat: obtain closest tag and tag commit sha

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,83 @@
-use std::{process::{Command, exit}, io::Error, io::ErrorKind};
-
+use std::{
+    io::Error,
+    io::ErrorKind,
+    process::{exit, Command},
+};
 
 fn verify_git() -> Result<bool, Error> {
-    Command::new("git")
-        .arg("--version")
-        .output()?;
+    Command::new("git").arg("--version").output()?;
 
     let output = Command::new("git")
         .arg("rev-parse")
         .arg("--is-inside-work-tree")
         .output()?;
-    
+
     if !output.status.success() {
-        return Err(Error::new(ErrorKind::Other, "Not in a git working tree"));
+        return Err(Error::new(ErrorKind::Other, "not in a git working tree"));
     }
     Ok(true)
 }
 
-fn get_closes_tag() -> Result<String, Error> {
+fn get_closest_tag() -> Result<String, Error> {
     let output = Command::new("git")
         .arg("describe")
-        .arg("--abrev=0")
+        .arg("--abbrev=0")
         .arg("--tags")
         .output()?;
+
+    if !output.status.success() {
+        let stderr = String::from_utf8_lossy(&output.stderr).to_string();
+        return Err(Error::new(
+            ErrorKind::Other,
+            format!(
+                "can not get closest tag: {} - code: {}",
+                stderr,
+                output.status.code().unwrap()
+            ),
+        ));
+    }
+
+    let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+    Ok(String::from(stdout.strip_suffix("\n").unwrap()))
 }
 
+fn get_tag_commit_sha(tag: &String) -> Result<String, Error> {
+    let output = Command::new("git")
+        .arg("rev-list")
+        .arg("-n 1")
+        .arg(tag)
+        .output()?;
+
+    if !output.status.success() {
+        let stderr = String::from_utf8_lossy(&output.stderr).to_string();
+        return Err(Error::new(
+            ErrorKind::Other,
+            format!(
+                "can not get tag commit sha: {} - code: {}",
+                stderr,
+                output.status.code().unwrap()
+            ),
+        ));
+    }
+
+    let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+    Ok(String::from(stdout.strip_suffix("\n").unwrap()))
+}
 
 fn main() {
     match verify_git() {
-        Ok(_) => println!("Git verification passed"),
         Err(error) => {
             println!("Git verification not passed, error: {}", error);
             exit(1);
-        }   
+        }
+        _ => {}
     }
-}
 
+    let tag = get_closest_tag().expect("failed to get closest tag");
+    println!("tag: {}", tag);
+    let tag_commit_sha = get_tag_commit_sha(&tag).expect("failed to get tag commit sha");
+    println!("tag {} - commit: {}", tag, tag_commit_sha);
+}
 
 // Obtain closest tag: git describe --abbrev=0 --tags
 // Obtain commit sha from the latest tag: git rev-list -n 1 0.2.2
