9186379db3588c8ce407626b45efb8c2f58fc10b
lopezd <david.lopez@creamteam.de>
2023-09-12 10:58:39 +0200

    style: changes discovered while docummented code

--- src/source/mod.rs
+++ src/source/mod.rs
@@ -1,16 +1,60 @@
+//! This module includes the different kinds of sources that can be used
+//! to get the required data to calculate a version bump.
+//!
+//! List of sources:
+//! - `git`: uses the `git` command and git history as the source of truth.
+//! - `github`: uses the GitHub REST API as the source of truth.
+//!
+
 use crate::error::Error;
 use enum_dispatch::enum_dispatch;
 
 pub mod git;
 pub mod github;
 
+/// Trait to describe all common actions that all sources need to implement.
 #[enum_dispatch]
 pub trait SourceActions {
+    /// Returns commit messages. This function can not be called without `fetch_from_commit` being called before
+    /// as that is the function that feeds tag track with data from the source.
+    ///
+    /// # Errors
+    ///
+    /// Returns `error::Error` with the type of `error::ErrorKind::SourceNotFetched` if the function is being
+    /// called without calling `fetch_from_commit` before.
+    ///
     fn get_commit_messages(&self) -> Result<&Vec<String>, Error>;
-    fn get_closest_tag(&self) -> Result<&String, Error>;
+
+    /// Returns the oldest closest git tag. This function can not be called without `fetch_from_commit` being called before
+    /// as that is the function that feeds tag track with data from the source.
+    ///
+    /// # Errors
+    ///
+    /// Returns `error::Error` with the type of `error::ErrorKind::SourceNotFetched` if the function is being
+    /// called without calling `fetch_from_commit` before.
+    ///
+    fn get_closest_oldest_tag(&self) -> Result<&String, Error>;
+
+    /// Fetches the source to gather the required data to calculate a version bump, this data can be obtained differently depending
+    /// on the source kind.
+    ///
+    /// # Arguments
+    ///
+    /// * `sha` - git commit SHA from where the commits should be obtained. This SHA typically corresponds to the current
+    /// HEAD commit.
+    ///
+    /// # Errors
+    ///
+    /// Returns `error::Error` with specific kinds depending of the source if there was an error while fetching the data.
+    ///
     fn fetch_from_commit(&mut self, sha: &str) -> Result<(), Error>;
 }
 
+/// Type used to wrap different source kinds.
+///
+/// This type uses the `enum_dispatch` macro to automatically implement the `SourceActions` trait, avoiding
+/// to manually implementing the trait with a match statement calling the function depending on the source kind.
+///
 #[enum_dispatch(SourceActions)]
 pub enum SourceKind {
     Git(git::GitSource),
--- src/version.rs
+++ src/version.rs
@@ -1,29 +1,60 @@
+//! Module containing version related help functions and types to define version increments.
+//!
+//! Version increments follow the Semantic Versioning 2.0
+//!
+
 use semver::{BuildMetadata, Prerelease, Version};
 
+/// Types for different version.
+/// The increment types follow the Semantic Version specification.
 #[derive(Eq, PartialEq, Hash)]
 pub enum IncrementKind {
+    /// Increment the major section of a version.
     Major,
+    /// Increment the minor section of a version.
     Minor,
+    /// Increment the patch section of a version.
     Patch,
 }
 
-pub fn increment_patch(v: &mut Version) {
-    v.patch += 1;
-    v.pre = Prerelease::EMPTY;
-    v.build = BuildMetadata::EMPTY;
+/// Increments the patch section of a `semver::Version`. This also empties the pre-release
+/// and build sections.
+///
+/// # Arguments
+///
+/// * `version` - Version that will be modified.
+///
+pub fn increment_patch(version: &mut Version) {
+    version.patch += 1;
+    version.pre = Prerelease::EMPTY;
+    version.build = BuildMetadata::EMPTY;
 }
 
-pub fn increment_minor(v: &mut Version) {
-    v.minor += 1;
-    v.patch = 0;
-    v.pre = Prerelease::EMPTY;
-    v.build = BuildMetadata::EMPTY;
+/// Increments the minor section of a `semver::Version`. This also empties the pre-release
+/// and build sections, and sets the patch section to 0.
+///
+/// # Arguments
+///
+/// * `version` - Version that will be modified.
+///
+pub fn increment_minor(version: &mut Version) {
+    version.minor += 1;
+    version.patch = 0;
+    version.pre = Prerelease::EMPTY;
+    version.build = BuildMetadata::EMPTY;
 }
 
-pub fn increment_major(v: &mut Version) {
-    v.major += 1;
-    v.minor = 0;
-    v.patch = 0;
-    v.pre = Prerelease::EMPTY;
-    v.build = BuildMetadata::EMPTY;
+/// Increments the major section of a `semver::Version`. This also empties the pre-release
+/// and build sections, and sets the patch and minor section to 0.
+///
+/// # Arguments
+///
+/// * `version` - Version that will be modified.
+///
+pub fn increment_major(version: &mut Version) {
+    version.major += 1;
+    version.minor = 0;
+    version.patch = 0;
+    version.pre = Prerelease::EMPTY;
+    version.build = BuildMetadata::EMPTY;
 }
