1947c798d8fce1197a74200f5236c6cbe6a2b5e6
David Lopez <davidlopez.hellin@outlook.com>
2023-10-22 23:11:50 +0200

    feat: (WIP) return `CommitIterator` instead of list of commits

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use source::github::GithubSource;
+use source::{github::GithubSource, SourceActions};
 
 mod config;
 mod error;
 mod git;
+mod parsing;
 mod source;
 mod version;
 
@@ -13,7 +14,18 @@ fn main() {
 
     let mut config = config::Config::new();
     config.tag_pattern = String::from("(?<scope>.*)/(?<version>.*)");
-    config.version_scopes = vec![String::from("example1"), String::from("example2s")];
+    config.version_scopes = vec![String::from("example1"), String::from("example2")];
 
-    let source = GithubSource::new(repo_id, api_url, token, &config);
+    let mut source = GithubSource::new(repo_id, api_url, token, &config);
+    let commits = match source.get_commits("069faa2c784049fc9e9f97a57579059295dd8236") {
+        Ok(commits) => commits,
+        Err(e) => {
+            println!("Error: {}", e);
+            return;
+        }
+    };
+
+    for commit in commits {
+        println!("Commit: {:?}", commit);
+    }
 }
