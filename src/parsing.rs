708e7e9c4d5f315f295a4607d894e8d659b0e9b8
David Lopez <davidlopez.hellin@outlook.com>
2023-10-23 23:25:30 +0200

    feat: (WIP) support commits with multiple tags

--- src/parsing.rs
+++ src/parsing.rs
@@ -1,6 +1,18 @@
 use crate::error::{Error, ErrorKind};
-use crate::git::Commit;
+use crate::git::{Commit, Tag};
 use regex::Regex;
+use semver::Version;
+
+/// Regex capturing group name for the type of the commit.
+const TYPE_CAPTURING_GROUP_NAME: &str = "type";
+/// Regex capturing group name for the scope of the commit.
+const SCOPE_CAPTURING_GROUP_NAME: &str = "scope";
+/// Regex capturing group name for the breaking change indicator of the commit.
+const BREAKING_CAPTURING_GROUP_NAME: &str = "breaking";
+/// Regex capturing group name for the description of the commit.
+const DESCRIPTION_CAPTURING_GROUP_NAME: &str = "description";
+/// Regex capturing group name for the version inside a tag.
+const VERSION_CAPTURING_GROUP_NAME: &str = "version";
 
 /// Type to represent the sections of a conventional commit message.
 #[derive(Debug)]
@@ -9,7 +21,7 @@ pub struct CommitDetails {
     pub commit_type: String,
 
     /// The scope of the commit.
-    pub scope: String,
+    pub scope: Option<String>,
 
     /// If the commit includes a breaking change. Typically this is true if the commit type includes the `!` char.
     pub breaking: bool,
@@ -17,15 +29,6 @@ pub struct CommitDetails {
     /// The description of the conventional commit.
     pub description: String,
 }
-
-/// Regex field name for the type of the commit.
-const TYPE_FIELD: &str = "type";
-/// Regex field name for the scope of the commit.
-const SCOPE_FIELD: &str = "scope";
-/// Regex field name for the breaking change indicator of the commit.
-const BREAKING_FIELD: &str = "breaking";
-/// Regex field name for the description of the commit.
-const DESCRIPTION_FIELD: &str = "description";
 
 /// Extracts the commit details from a commit message.
 ///
@@ -69,7 +72,7 @@ pub fn extract_commit_details(
         }
     };
 
-    let commit_type = match captures.name(TYPE_FIELD) {
+    let commit_type = match captures.name(TYPE_CAPTURING_GROUP_NAME) {
         None => {
             return Err(Error::new(
                 ErrorKind::InvalidCommitPattern,
@@ -79,18 +82,20 @@ pub fn extract_commit_details(
         Some(found_match) => found_match.as_str().trim().to_string(),
     };
 
-    let scope = match captures.name(SCOPE_FIELD) {
-        None => "".to_string(),
-        Some(found_match) => found_match
-            .as_str()
-            .replace(['(', ')'], "")
-            .trim()
-            .to_string(),
+    let scope = match captures.name(SCOPE_CAPTURING_GROUP_NAME) {
+        None => None,
+        Some(found_match) => Some(
+            found_match
+                .as_str()
+                .replace(['(', ')'], "")
+                .trim()
+                .to_string(),
+        ),
     };
 
-    let breaking = captures.name(BREAKING_FIELD).is_some();
+    let breaking = captures.name(BREAKING_CAPTURING_GROUP_NAME).is_some();
 
-    let description = match captures.name(DESCRIPTION_FIELD) {
+    let description = match captures.name(DESCRIPTION_CAPTURING_GROUP_NAME) {
         None => {
             return Err(Error::new(
                 ErrorKind::InvalidCommitPattern,
@@ -114,4 +119,63 @@ pub fn extract_commit_details(
         breaking,
         description,
     })
+}
+
+/// Type to represent the sections of a tag.
+#[derive(Debug, Clone)]
+pub struct TagDetails {
+    /// The version inside the tag.
+    pub version: Version,
+
+    /// The scope of the version.
+    pub scope: Option<String>,
+}
+
+pub fn extract_tag_details(tag: &Tag, tag_pattern: &str) -> Result<TagDetails, Error> {
+    let re = match Regex::new(tag_pattern) {
+        Ok(re) => re,
+        Err(error) => {
+            return Err(Error::new(
+                ErrorKind::InvalidRegexPattern,
+                Some(format!("{} - {}", tag_pattern, error.to_string().as_str()).as_str()),
+            ))
+        }
+    };
+
+    let captures = match re.captures(&tag.name) {
+        Some(captures) => captures,
+        None => {
+            return Err(Error::new(
+                ErrorKind::InvalidTagPattern,
+                Some(format!("tag {} does not match pattern {}", tag.name, tag_pattern).as_str()),
+            ))
+        }
+    };
+
+    let version = match captures.name(VERSION_CAPTURING_GROUP_NAME) {
+        None => {
+            return Err(Error::new(
+                ErrorKind::InvalidTagPattern,
+                Some("missing version inside tag"),
+            ))
+        }
+        Some(found_match) => {
+            let version = found_match.as_str().trim().to_string();
+            let version = Version::parse(&version)?;
+            version
+        }
+    };
+
+    let scope = match captures.name(SCOPE_CAPTURING_GROUP_NAME) {
+        None => None,
+        Some(found_match) => Some(
+            found_match
+                .as_str()
+                .replace(['(', ')'], "")
+                .trim()
+                .to_string(),
+        ),
+    };
+
+    Ok(TagDetails { version, scope })
 }
